38bef1b7781d4020f4d2f954419de41d822bb86d
vlada <zinasoska137@gmail.comtimofeevavlada60@gmail.com>
2022-10-07 00:15:14 +0400

    add homework 06.10

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double r, s, l;
+    cin >> r;
+    s = 3.14 * r * r;
+    l = 2 * 3.14 * r;
+    cout << "S=" << s << " L=" << l;
+
+}
