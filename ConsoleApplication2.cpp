7bc42d9f7077429a241e30a3d374a3109c8afeac
vlada <zinasoska137@gmail.comtimofeevavlada60@gmail.com>
2022-10-07 00:34:01 +0400

    add homework 06.10

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b, c, p, s;
+    cout << "Введите стороны треугольника" << "\n";
+    cin >> a >> b >> c;
+    p = (a + b + c) / 2;
+    s = sqrt(p * (p - a) * (p - b) * (p - c));
+    cout << "Площадь треугольника равна " << s;
+}
