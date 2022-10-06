ffd251db2d4703a41c95f27e389681afa75e0d9b
vlada <zinasoska137@gmail.comtimofeevavlada60@gmail.com>
2022-10-07 01:21:35 +0400

    add homework 06.10

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, x;
+    cout << "Введите значение x:\n";
+    cin >> x;
+    a = pow(cosh(x), -7) - (log10(2 * x)) / log10(7.0 / 8.0);
+    cout << "Итоговое значение:\n" << a;
+}
