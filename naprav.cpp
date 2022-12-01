dac66a3e22108f3bda1fdac35e57599449ac13bd
blurrrn <103361796+blurrrn@users.noreply.github.com>
2022-12-01 23:40:16 +0400

    add homework 14.10

---
+++ naprav.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cin >> n;
+    switch (n) {
+    case 1:
+        cout << "Влево (Горизонтально)";
+        break;
+    case 2:
+        cout << "Вверх (Вертикально)";
+        break;
+    case 3:
+        cout << "Вправо (Горизонтально)";
+        break;
+    case 4:
+        cout << "Вниз (Вертикально)";
+        break;
+    }
+}
---
+++ pervotriz.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int x, n = 1;
+    while (true) {
+        cin >> x;
+        n++;
+        if (x < 0) {
+            cout << n - 1;
+            break;
+        }
+    }
+}
