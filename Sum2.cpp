bdd6c3c52cc9231a065757f3ca16293be83192e2
blurrrn <103361796+blurrrn@users.noreply.github.com>
2022-12-07 00:04:55 +0400

    add homework 02.12.22

---
+++ Sum2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+typedef long long l;
+
+int main() {
+    double x, s = 0, p_x;
+    l k, f = 1;
+    cin >> k >> x;
+    p_x = x;
+    for (l n = 1; n <= k; n++) {
+        f *= n;
+        p_x *= x;
+        p_x *= x;
+        s += p_x * f;
+    }
+    cout << s;
+}
---
+++ Sum3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+typedef long long l;
+double eps = 1e-9;
+
+double binpow(double x, l n) {
+    double ans = 1;
+    while (n) {
+        if (n & 1) {
+            ans *= x;
+            --n;
+        }
+        else {
+            x *= x;
+            n >>= 1;
+        }
+    }
+    return ans;
+}
+void fast_factor(l& n) {
+    for (l i = n + 1; i <= 2 * n; i++) {
+        n *= i;
+    }
+}
+int main() {
+    double curr, x;
+    l n = 1, k = 1;
+    for (int i = n; i < 10; i++) {
+        fast_factor(n);
+        cout << 2 * k << " " << n << " ";
+        n *= 2;
+        k++;
+    }
+}
