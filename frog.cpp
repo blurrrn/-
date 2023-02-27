68dc331461fdca6e9d31008a21842f1591a8b72e
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-02-28 01:47:46 +0400

    add homework 28.02.23

---
+++ frog.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int fra(int n, int x) {
+    if (n > x)
+        return 0;
+    else if (n == x)
+        return 1;
+    else if (n < x)
+        return fra(n + 3, x) + fra(n + 5, x);
+}
+
+int main() {
+    int x, n = 0;
+    cin >> x;
+    cout << fra(n, x);
+}
